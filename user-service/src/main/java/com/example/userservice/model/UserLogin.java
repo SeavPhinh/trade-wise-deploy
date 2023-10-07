a3e5aef37a691fdf2b7a21f03afde10ee691d672
IamSak <renrithysak095@gmail.com>
2023-10-07 23:12:13 +0700

    throw exception completed

--- user-service/src/main/java/com/example/userservice/model/UserLogin.java
+++ user-service/src/main/java/com/example/userservice/model/UserLogin.java
@@ -2,8 +2,6 @@ package com.example.userservice.model;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
