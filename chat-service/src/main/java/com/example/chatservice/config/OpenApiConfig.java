10e22315bd0b72145f8a662cd69c3b2adb86ba58
IamSak <renrithysak095@gmail.com>
2023-10-31 22:04:02 +0700

    developer

--- chat-service/src/main/java/com/example/chatservice/config/OpenApiConfig.java
+++ chat-service/src/main/java/com/example/chatservice/config/OpenApiConfig.java
@@ -23,7 +23,7 @@ import io.swagger.v3.oas.annotations.servers.Server;
         in = SecuritySchemeIn.HEADER,
         flows = @OAuthFlows(
                 clientCredentials = @OAuthFlow(
-                        tokenUrl = "http://localhost:1234/auth/realms/go-selling-api/protocol/openid-connect/token"
+                        tokenUrl = "https://keycloak.trade-wise.online/auth/realms/go-selling-api/protocol/openid-connect/token"
                 )
         )
 )
