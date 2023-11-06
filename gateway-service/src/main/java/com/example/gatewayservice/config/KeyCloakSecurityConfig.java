b0d6878172a51e0678eb547e6c8bde859baf00f1
IamSak <renrithysak095@gmail.com>
2023-11-06 09:56:07 +0700

    rithysak

--- gateway-service/src/main/java/com/example/gatewayservice/config/KeyCloakSecurityConfig.java
+++ gateway-service/src/main/java/com/example/gatewayservice/config/KeyCloakSecurityConfig.java
@@ -7,7 +7,9 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.reactive.CorsConfigurationSource;
 import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
@@ -16,38 +18,23 @@ import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 @Configuration
 public class KeyCloakSecurityConfig {
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .csrf(AbstractHttpConfigurer::disable)
-                .cors(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(authorize -> {
-                    //For OpenAPI
-                    authorize.requestMatchers("user-service/v3/api-docs/**", "user-service/swagger-ui/**", "user-service/swagger-ui.html").permitAll();
-                    authorize.requestMatchers(HttpMethod.GET,"/api/v1/users").permitAll();
-                    authorize.requestMatchers(HttpMethod.POST,"/api/v1/users").permitAll();
-                    authorize.requestMatchers("/api/v1/third-party/modify").permitAll();
-                    authorize.requestMatchers("/api/v1/users/login").permitAll();
-                    authorize.requestMatchers("/api/v1/users/username").permitAll();
-                    authorize.requestMatchers("/api/v1/users/verify").permitAll();
-                    authorize.requestMatchers("/api/v1/users/reset-password").permitAll();
-                    authorize.requestMatchers("/api/v1/users/users/email").permitAll();
-                    authorize.requestMatchers(HttpMethod.POST,"/api/v1/users/otp/reset-password").permitAll();
-                    authorize.requestMatchers(HttpMethod.GET,"/api/v1/users/{id}").permitAll();
-                    authorize.anyRequest().authenticated();
-                }).oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults()));
-        return http.build();
+    public SecurityWebFilterChain securityFilter(ServerHttpSecurity http) {
+        return http.authorizeExchange()
+                .pathMatchers("/tasks/**", "/users/**",
+                        "/tasks/api/v1/tasks/**",
+                        "/users/api/v1/users/**",
+                        "/actuator/**",
+                        "/webjars/**", "/v3/api-docs/**",
+                        "/keycloak-client/v3/api-docs",
+                        "/task-service/v3/api-docs").permitAll()
+                .anyExchange().permitAll()
+                .and()
+                .cors().and()
+                .csrf().disable()
+                .oauth2ResourceServer()
+                .jwt().and()
+                .and()
+                .build();
     }
 
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.addAllowedOrigin("*");
-        configuration.addAllowedMethod("*");
-        configuration.addAllowedHeader("*");
-        configuration.setAllowCredentials(true);
-
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
-    }
 }
