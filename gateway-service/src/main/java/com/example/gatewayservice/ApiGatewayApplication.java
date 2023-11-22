cb5720e60e65ed84e9e2c1fed974eafc7841f78f
Seav Phinh <seavpinhkao@gmail.com>
2023-11-22 14:35:49 +0700

    add files

--- gateway-service/src/main/java/com/example/gatewayservice/ApiGatewayApplication.java
+++ gateway-service/src/main/java/com/example/gatewayservice/ApiGatewayApplication.java
@@ -9,10 +9,10 @@ import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class GatewayServiceApplication {
+public class ApiGatewayApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(GatewayServiceApplication.class, args);
+        SpringApplication.run(ApiGatewayApplication.class, args);
     }
 
     @Bean
